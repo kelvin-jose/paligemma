769c95bf8b2ad11f13452ba588473457fb8bb118
Kelvin <kelvinkonoor@gmail.com>
2025-02-14 12:13:26 +0530

    preprocessing pipeline for PIL images, 1. resize 2. rescale 3. norm 4. tranpose

--- processor.py
+++ processor.py
@@ -1,7 +1,14 @@
+import numpy as np
+from PIL import Image
+
 class PaliGemmaProcessor:
-    def __init__(self, tokenizer):
-        self.image_token = '<image>'
+    image_token = '<image>'
+    mean = np.array([0.5, 0.5, 0.5], dtype=np.float32)
+    std = np.array([0.5, 0.5, 0.5], dtype=np.float32)
+
+    def __init__(self, tokenizer, image_size):
         self.tokenizer = tokenizer
+        self.image_size = image_size
         self.set_tokens()
     
     def set_tokens(self):
@@ -12,3 +19,14 @@ class PaliGemmaProcessor:
         self.tokenizer.add_tokens(extra_tokens)
         self.tokenizer.add_bos_token = False
         self.tokenizer.add_eos_token = False
+
+    def preprocess_image(self, images):
+        for image in images:
+            image = image.resize((self.image_size, self.image_size))
+            image = np.array(image) * (1/255.).astype(np.float32)
+            image = (image - self.mean) / self.std
+            image = image.transpose(2, 0, 1)
+        return images
+
+    def __call__(self, images, texts):
+        images = self.preprocess_image(images)
