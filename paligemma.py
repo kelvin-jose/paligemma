88e465f8238ad01367f012e3342f88ccd64ffc48
Kelvin <kelvinkonoor@gmail.com>
2025-03-04 22:18:14 +0530

    vision_tower is out SiGLIP implementation

--- paligemma.py
+++ paligemma.py
@@ -7,3 +7,8 @@ import torch.nn as nn
 from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
+
+class PaliGemma(nn.Module):
+    def __init__(self, vision_config, language_config):
+        super().__init__()
+        self.vision_tower = siglip.SigLIP(siglip.SigLIPConfig)
