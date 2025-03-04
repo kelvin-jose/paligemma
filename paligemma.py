bdc211d6b03d32e486eeb53fc39672ee5ca2f1b4
Kelvin <kelvinkonoor@gmail.com>
2025-03-04 22:17:36 +0530

    other necessary imports

--- paligemma.py
+++ paligemma.py
@@ -1,3 +1,9 @@
 import gemma
 import siglip
 from processor import PaliGemmaProcessor
+
+import torch
+import torch.nn as nn
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b")
